c5ee946ca95f9281cdf84ca328c70437b8597c89
Nikita <nikitabechthold@gmail.com>
2023-07-07 11:11:35 +0600

    Configured Token-based auth

--- heroes_inn/heroes_inn/urls.py
+++ heroes_inn/heroes_inn/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 # from .views import home_view
@@ -25,6 +25,8 @@ urlpatterns = [
     # path('', home_view, name='home'),
     path('admin/', admin.site.urls),
     path('heroes/', include('heroes.urls')),
+    re_path(r'^auth/', include('djoser.urls')),
+    re_path(r'^auth/', include('djoser.urls.authtoken')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
